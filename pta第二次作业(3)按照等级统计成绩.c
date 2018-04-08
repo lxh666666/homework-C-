c77ca64ae3634ba2fef7c8a864b0e421823aa96d
lxh666666 <37371011+lxh666666@users.noreply.github.com>
2018-04-08 20:42:06 +0800

    Add files via upload

---
+++ pta第二次作业(3)按照等级统计成绩.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define MAXN 10
+
+struct student{
+    int num;
+    char name[20];
+    int score;
+    char grade;
+};
+
+int set_grade( struct student *p, int n );
+
+int main()
+{   struct student stu[MAXN], *ptr;
+    int n, i, count;
+
+    ptr = stu;
+    scanf("%d\n", &n);
+    for(i = 0; i < n; i++){
+       scanf("%d%s%d", &stu[i].num, stu[i].name, &stu[i].score);
+    } 
+   count = set_grade(ptr, n);
+   printf("The count for failed (<60): %d\n", count);
+   printf("The grades:\n"); 
+   for(i = 0; i < n; i++)
+       printf("%d %s %c\n", stu[i].num, stu[i].name, stu[i].grade);
+    return 0;
+}
+int set_grade( struct student *p, int n ){
+	int count = 0, i;
+	for(i = 0;i<n;i++,p++){
+		if(p->score<60){
+			p->grade = 'D';
+			count++;
+		}
+		else if((p->score<70)&&(p->score>=60)){
+			p->grade = 'C';
+		}
+		else if((p->score<85)&&(p->score>=70)){
+			p->grade = 'B';
+		}
+		else{
+			p->grade = 'A';
+		}
+	}
+	return count;
+}
